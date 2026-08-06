aab9df5070232a384e9ac63161ea54a59797bfdf
Luis Sousa <luis.sousa201212@gmail.com>
2026-08-06 14:51:05 +0000

    External agent changes

--- src/components/form/AcumuloSection.jsx
+++ src/components/form/AcumuloSection.jsx
@@ -3,7 +3,7 @@ import SectionCard from "@/components/form/SectionCard";
 import { TextArea, YesNo } from "@/components/form/FormFields";
 
 export default function AcumuloSection({ data, onChange, onChoice }) {
-  return <SectionCard number="12" title="Acúmulo/desvio de função"><div className="space-y-7">
+  return <SectionCard number="13" title="Acúmulo/desvio de função"><div className="space-y-7">
     <YesNo label="Houve acúmulo ou desvio de função?" name="acumulo_funcao" value={data.acumulo_funcao} onChange={onChoice} />
     <TextArea label="Quais funções" name="funcoes_acumuladas" value={data.funcoes_acumuladas} onChange={onChange} rows={3} />
   </div></SectionCard>;
