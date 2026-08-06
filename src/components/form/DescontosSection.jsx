b3ffa5c1770377e61a1d8cbf8e8fb6da9ac4f8fa
Luis Sousa <luis.sousa201212@gmail.com>
2026-08-06 14:52:15 +0000

    External agent changes

--- src/components/form/DescontosSection.jsx
+++ src/components/form/DescontosSection.jsx
@@ -3,7 +3,7 @@ import SectionCard from "@/components/form/SectionCard";
 import { Field, YesNo } from "@/components/form/FormFields";
 
 export default function DescontosSection({ data, onChange, onChoice }) {
-  return <SectionCard number="16" title="Descontos"><div className="space-y-7">
+  return <SectionCard number="17" title="Descontos"><div className="space-y-7">
     <YesNo label="Houve desconto indevido?" name="desconto_indevido" value={data.desconto_indevido} onChange={onChoice} />
     <Field label="Qual" name="desconto_qual" value={data.desconto_qual} onChange={onChange} />
   </div></SectionCard>;
