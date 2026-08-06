2c48e0719e3b40b4b0dd04f3c088161b14a1dc6a
Luis Sousa <luis.sousa201212@gmail.com>
2026-08-06 14:43:44 +0000

    External agent changes

--- src/components/form/DispensaSection.jsx
+++ src/components/form/DispensaSection.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import SectionCard from "@/components/form/SectionCard";
-import { Field, Select } from "@/components/form/FormFields";
+import { Select } from "@/components/form/FormFields";
 import { TIPO_DISPENSA_OPTIONS } from "@/lib/interviewOptions";
 
 export default function DispensaSection({ data, onChange }) {
-  return <SectionCard number="1" title="Tipo de dispensa"><div className="grid gap-5 sm:grid-cols-2">
+  return <SectionCard number="5" title="Tipo de dispensa"><div className="grid gap-5 sm:grid-cols-2">
     <Select label="Tipo de dispensa" name="tipo_dispensa" value={data.tipo_dispensa} options={TIPO_DISPENSA_OPTIONS} onChange={onChange} />
-    <Field label="Último dia trabalhado" name="ultimo_dia" value={data.ultimo_dia} onChange={onChange} type="date" />
   </div></SectionCard>;
 }
