7c1c74052b221766e4589971afa183502992696b
Luis Sousa <luis.sousa201212@gmail.com>
2026-08-06 14:51:57 +0000

    External agent changes

--- src/components/form/DocumentosSection.jsx
+++ src/components/form/DocumentosSection.jsx
@@ -3,7 +3,7 @@ import SectionCard from "@/components/form/SectionCard";
 import { YesNo } from "@/components/form/FormFields";
 
 export default function DocumentosSection({ data, onChoice }) {
-  return <SectionCard number="15" title="Documentos"><div className="grid gap-7 sm:grid-cols-3">
+  return <SectionCard number="16" title="Documentos"><div className="grid gap-7 sm:grid-cols-3">
     <YesNo label="Holerites" name="holerites" value={data.holerites} onChange={onChoice} />
     <YesNo label="Rescisão contratual" name="rescisao_contratual" value={data.rescisao_contratual} onChange={onChoice} />
     <YesNo label="Espelho de ponto" name="espelho_ponto" value={data.espelho_ponto} onChange={onChoice} />
