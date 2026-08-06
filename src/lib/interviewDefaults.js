4e43175cd61821be4cfc829a83aabffc956ffe1b
Luis Sousa <luis.sousa201212@gmail.com>
2026-08-06 14:55:03 +0000

    External agent changes

--- src/lib/interviewDefaults.js
+++ src/lib/interviewDefaults.js
@@ -1,40 +1,78 @@
 export const interviewDefaults = {
   modelo_peticao: "",
-  nome_cliente: "MARCOS MOREIRA PAULO",
-  nacionalidade: "brasileiro",
-  estado_civil: "solteiro",
-  rg: "672853966 São Paulo/SP",
-  cpf: "105.678.257-95",
-  pis: "1290165260-5",
-  ctps: "105678, série 25795",
-  data_nascimento: "1983-07-08",
-  filiacao: "José Paulo Irmão e Damiana Moreira Paulo",
-  endereco_cliente: "Rua Antonio de Albuquerque, nº 181, cs 03, Aldeinha, Itapecerica da Serra/SP, CEP 06877-150",
+  titulo: "",
+  RECL_NOME: "MARCOS MOREIRA PAULO",
+  RECL_NACIONALIDADE: "brasileiro",
+  RECL_ESTADOCIVIL: "solteiro",
+  RECL_RG: "672853966 São Paulo/SP",
+  RECL_CPF: "105.678.257-95",
+  RECL_PIS: "1290165260-5",
+  RECL_CTPS: "105678",
+  RECL_SERIE: "25795",
+  RECL_NASC: "1983-07-08",
+  RECL_FILIACAO: "José Paulo Irmão e Damiana Moreira Paulo",
+  RECL_ENDERECO: "Rua Antonio de Albuquerque, nº 181, cs 03, Aldeinha, Itapecerica da Serra/SP",
+  RECL_CEP: "06877-150",
   email: "marcos81769111@gmail.com",
   telefone: "11 97419-4828 ou esposa 11 99910-8357",
-  admissao: "2025-04-14",
-  demissao: "2025-12-07",
-  salario: "",
-  reclamadas: [
-    { razao_social: "VIGSEG VIGILÂNCIA E SEGURANÇA DE VALORES LTDA", cnpj: "04.542.518/0002-99", endereco: "PRQ DOMINGOS LUIS 699, 2043081, JARDIM SÃO PAULO (ZONA NORTE), SP", cargo: "VIGILANTE", escala: "12x36" },
-    { razao_social: "GLP RÉGIS (Integral Médica)", cnpj: "46.652.606/0001-02", endereco: "Rod. Régis Bittencourt, s/n, km 296,5, Itaguaciara, Itapecerica da Serra/SP, CEP 06877-115", cargo: "", escala: "12x36" }
-  ],
-  tipo_dispensa: "Sem justa causa",
-  ultimo_dia: "",
+
+  RECL1_NOME: "VIGSEG VIGILÂNCIA E SEGURANÇA DE VALORES LTDA",
+  RECL1_CNPJ: "04.542.518/0002-99",
+  RECL1_LOGRADOURO: "PRQ DOMINGOS LUIS 699, 2043081, JARDIM SÃO PAULO (ZONA NORTE)",
+  RECL1_ENDCOMPL: "São Paulo/SP",
+  RECL2_NOME: "GLP RÉGIS (Integral Médica)",
+  RECL2_CNPJ: "46.652.606/0001-02",
+  RECL2_LOGRADOURO: "Rod. Régis Bittencourt, s/n, km 296,5, Itaguaciara",
+  RECL2_ENDCOMPL: "Itapecerica da Serra/SP, CEP 06877-115",
+  RECL3_NOME: "", RECL3_CNPJ: "", RECL3_LOGRADOURO: "", RECL3_ENDCOMPL: "",
+
+  FUNCAO: "Vigilante",
+  escala: "12x36",
+  LOCAL_PRESTACAO: "",
+  LOCAL_PRESTACAO_COMPL: "",
+
+  COMARCA_UF: "", REGIAO_TRT: "", FORO_COMPETENCIA: "",
+
+  DATA_ADMISSAO: "2025-04-14",
+  DATA_RESCISAO: "2025-12-07",
+  SALARIO: "",
+
+  tipo_dispensa: "sem_justa_causa",
+
+  JORNADA_HORARIO: "18:30 às 07:30",
+  JORNADA_EXTRAPOLA: "",
+  JORNADA_FREQ_EXTRA: "",
+  tem_adic_noturno: true,
+
+  DANO_SUPERVISOR: "", DANO_FATOS: "", dano_sem_estrutura: false,
+
   vale_refeicao: true, vale_alimentacao: true, vale_transporte: true,
-  jornada: "18:30 às 07:30",
-  finais_semana: true,
+
   ferias: true, ferias_quantidade: "Proporcional",
-  folgas_trabalhadas: true, ft_quantidade: "5 a 6", ft_valor: "R$ 180 a R$ 200", ft_pagamento: "PIX", ft_comprovante: false,
-  intervalo_suprimido: true, intervalo_detalhes: "Rádio HT sempre ligado",
+
+  folgas_trabalhadas: true, FT_QTD_MEDIA: "5 a 6 por mês", VAL_FT: "R$ 180 a R$ 200", ft_pagamento: "PIX", ft_comprovante: false,
+  VAL_CONDUCAO: "", VAL_ALIMENTACAO: "",
+
+  intervalo_suprimido: true, INTERVALO_GOZADO: "Rádio HT sempre ligado",
+
   horas_extras: true, media_horas_extras: "Até 1 hora", periodo_antecedente: "30 minutos", periodo_sucedente: "30 minutos",
+  ADIC_CONV: "60%",
+
   acumulo_funcao: true, funcoes_acumuladas: "Prevenção de perdas: conferência de mercadorias, validade de produtos, registros operacionais, cargas, paletes e materiais",
+
   armamento_colete: false,
+
   gratificacao: false, gratificacao_qual: "",
+
   holerites: true, rescisao_contratual: false, espelho_ponto: false,
+
   desconto_indevido: true, desconto_qual: "Desconto integral do saldo de empréstimo consignado na rescisão",
-  doenca_acidente: false, insalubridade: false, periculosidade: true,
-  produtos: "", epi: "",
+
+  tem_doenca: false, tem_insalubridade: false, GRAU_INSALUBRIDADE: "", INSALUBRIDADE_FATOS: "",
+  tem_periculosidade: true, produtos: "", epi: "",
+
+  CCT_VIGENCIA: "", VALOR_CAUSA: "", LOCAL_DATA_ASSINATURA: "",
+
   testemunha: "Irá verificar",
   fatos_narrados: "Após o desligamento do colaborador responsável pela função de Prevenção de Perdas, passou a acumular, além de suas atividades habituais, as atribuições inerentes à referida função, sem qualquer contraprestação salarial adicional.\n\nRealizava conferências de mercadorias, controle e verificação de validade de produtos, registros operacionais, conferência de cargas, controle da quantidade de paletes, conferência de materiais e demais procedimentos relacionados ao setor de Prevenção. A acumulação gerava significativa sobrecarga de trabalho.\n\nRealizava em média de 5 a 6 folgas trabalhadas (FTs), sendo que os respectivos valores eram pagos fora da folha. Alega que não recebia PL.\n\nHavia solicitado empréstimo consignado durante a vigência do contrato de trabalho, tendo sido surpreendido com o desconto integral do saldo devedor na rescisão contratual."
 };
